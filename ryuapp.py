42f5ef4b475774ee3808f9e6954de2bbf8e1e760
alexl <alexl@unswengsoc.com>
2018-05-03 00:17:06 +1000

    Added PacketIn handler TODO: handle ARP

--- ryuapp.py
+++ ryuapp.py
@@ -4,6 +4,7 @@ from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
+import array
 
 class L2Switch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -37,10 +38,18 @@ class L2Switch(app_manager.RyuApp):
             flags=ofproto.OFPFF_SEND_FLOW_REM, instructions=instructions)
         dp.send_msg(mod)
 
+    def add_flow(self, dp, pri, match, actions):
+        instructions = [dp.ofproto_parser.OFPInstructionActions(dp.ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = dp.ofproto_parser.OFPFlowMod(
+            datapath=dp, match=match, cookie=0,
+            command=dp.ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
+            priority=pri, flags=dp.ofproto.OFPFF_SEND_FLOW_REM, instructions=instructions
+        )
+        dp.send_msg(mod)
+
     # @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
-    def _packet_in_handler(self, ev):
-        print 'Hello there'
+    def _packet_in_handler_init(self, ev):
         k = 4
         port_offset = 1
         msg = ev.msg
@@ -54,31 +63,67 @@ class L2Switch(app_manager.RyuApp):
         # Process switch based on DPID and build match/action list
         # print type(dpid)
         dpid_hex = '{:06x}'.format(dp.id)
-        print dpid_hex
-        spod = dpid_hex[:2]
-        print 'spod: ' + spod
-        swnum = dpid_hex[-2:-4:-1]
-        print 'swnum: ' + swnum
-        crnum = dpid_hex[-2:]
-        print 'crnum: ' + crnum
+        # print dpid_hex
+        spod = int(dpid_hex[:2], 16)
+        # print 'spod: ' + str(spod)
+        swnum = int(dpid_hex[-2:-4:-1], 16)
+        # print 'swnum: ' + str(swnum)
+        crnum = int(dpid_hex[-2:], 16)
+        # print 'crnum: ' + str(crnum)
 
-        pri = 1
+        match = dp.ofproto_parser.OFPMatch()
+        actions = [dp.ofproto_parser.OFPActionOutput(dp.ofproto.OFPP_CONTROLLER,
+                                                     dp.ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(dp, 0, match, actions)
 
-        for q in range(k / 2):
-            # Build suffix table entries
-            sufp = (((q - 2 + int(swnum)) % (k / 2)) + k / 2) + port_offset
-            ip = ('0.0.0.{}'.format(q + 2), '0.0.0.255')
-            print ip
-            self.add_flow_ip(dp, ip, pri, sufp)
+        # Core Switch Tables
+        if crnum == k:
+            # Do something
+            pri = 1
+            for i in range(k):
+                port = i + port_offset
+                ip = ('10.{}.0.0'.format(i), '255.255.0.0')
+                self.add_flow_ip(dp, ip, pri, port)
 
-        pri = 10
-        for q in range(k / 2):
-            # Build prefix table entries
-            prep = q + port_offset
-            ip = ('10.{}.{}.0'.format(int(spod), q), '255.255.255.0')
-            print ip
-            self.add_flow_ip(dp, ip, pri, prep)
+        # Aggregation Switch Tables
+        elif crnum < k and swnum >= k/2:
+            pri = 1
+            for q in range(k / 2):
+                # Build suffix table entries
+                sufp = (((q - 2 + swnum) % (k / 2)) + k / 2) + port_offset
+                ip = ('0.0.0.{}'.format(q + 2), '0.0.0.255')
+                self.add_flow_ip(dp, ip, pri, sufp)
 
+            pri = 10
+            for q in range(k / 2):
+                # Build prefix table entries
+                prep = q + port_offset
+                ip = ('10.{}.{}.0'.format(spod, q), '255.255.255.0')
+                self.add_flow_ip(dp, ip, pri, prep)
+
+        # Edge Switch Tables
+        elif crnum < k and swnum < k/2:
+            pri = 10
+            for i in range(k/2):
+                # Build host route entries
+                port = i + port_offset
+                ip = ('10.{}.{}.{}'.format(spod, swnum, i + 2))  # , '255.255.255.255')
+                self.add_flow_ip(dp, ip, pri, port)
+
+            pri = 1
+            for i in range(k/2):
+                # Build local pod subnet entries
+                port = (((i - 2 + swnum) % (k / 2)) + k / 2) + port_offset
+                ip = ('0.0.0.{}'.format(i+2), '0.0.0.255')
+                self.add_flow_ip(dp, ip, pri, port)
+
+        print('Pushing flow entry to switch {:x}'.format(dp.id))
         # actions = [ofp_parser.OFPActionOutput(ofp.OFPP_FLOOD)]
         # out = ofp_parser.OFPPacketOut(datapath=dp, buffer_id=msg.buffer_id, in_port=msg.in_port, actions=actions)
         # dp.send_msg(out)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        datapath = ev.msg.datapath
+        dpid = "{:06x}".format(datapath.id)
+        print('{} is receiving packets'.format(dpid))
